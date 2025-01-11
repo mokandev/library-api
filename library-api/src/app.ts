0680ada3f200da7c9480791d2ba8cb818264abf9
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 16:04:46 -0300

    feat: using app routes in app

--- library-api/src/app.ts
+++ library-api/src/app.ts
@@ -1,5 +1,8 @@
 import http, { IncomingMessage, ServerResponse } from 'http'
+import { appRoutes } from './http/routes'
 
-const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {})
+const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  appRoutes(req, res)
+})
 
 export default app
