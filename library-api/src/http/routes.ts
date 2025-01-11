801ed4133c4fc3f73dfbdfe9a98932064979865c
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 16:04:34 -0300

    feat: start to create appRoutes

---
+++ library-api/src/http/routes.ts
@@ -0,0 +1,12 @@
+import { IncomingMessage, ServerResponse } from 'http'
+import { listBooks } from './controllers/list-books'
+
+export async function appRoutes(req: IncomingMessage, res: ServerResponse) {
+  const { method } = req
+  const baseURL = `http://${req.headers.host}`
+  const { pathname } = new URL(req.url || '', baseURL)
+
+  if (pathname === '/api/v1/books' && method === 'GET') {
+    await listBooks(req, res)
+  }
+}
