b0a10f82b74656cb09f826d23240852a79463330
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 17:24:47 -0300

    feat: adds new method to books repository

--- library-api/src/repositories/prisma/prisma-books-repository.ts
+++ library-api/src/repositories/prisma/prisma-books-repository.ts
@@ -6,4 +6,14 @@ export class PrismaBooksRepository implements BooksRepository {
     const books = await prisma.book.findMany()
     return books
   }
+
+  async getBookDetails(bookId: string) {
+    const book = prisma.book.findUnique({
+      where: {
+        id: bookId,
+      },
+    })
+
+    return book
+  }
 }
