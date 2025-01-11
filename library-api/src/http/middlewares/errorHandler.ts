159d237e5348d5dc62d694e410684594a245e1df
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 17:27:25 -0300

    feat: creates a global error handler

---
+++ library-api/src/http/middlewares/errorHandler.ts
@@ -0,0 +1,22 @@
+import { env } from '@/env'
+import { IncomingMessage, ServerResponse } from 'http'
+import { ZodError } from 'zod'
+
+export const globalErrorHandler = (
+  error: Error,
+  _req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  if (error instanceof ZodError) {
+    res.writeHead(400)
+    return res.end(
+      JSON.stringify({ message: 'Validation error', issues: error.format() }),
+    )
+  } else {
+    if (env.NODE_ENV !== 'production') {
+      console.error(error)
+    }
+    res.writeHead(500)
+    return res.end(JSON.stringify({ message: 'Internal Server Error' }))
+  }
+}
