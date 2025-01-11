910feb01f7528183a950ab8e210fa55cc5260d9d
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 17:27:42 -0300

    feat: using the global error handler in app

--- library-api/src/app.ts
+++ library-api/src/app.ts
@@ -1,8 +1,15 @@
 import http, { IncomingMessage, ServerResponse } from 'http'
 import { appRoutes } from './http/routes'
+import { globalErrorHandler } from './http/middlewares/errorHandler'
 
-const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-  appRoutes(req, res)
-})
+const app = http.createServer(
+  async (req: IncomingMessage, res: ServerResponse) => {
+    try {
+      await appRoutes(req, res)
+    } catch (error) {
+      globalErrorHandler(error as Error, req, res)
+    }
+  },
+)
 
 export default app
