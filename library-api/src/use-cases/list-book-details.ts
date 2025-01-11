a3fee504826d75cc57753a57d9e4621b1f9ac509
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 17:25:44 -0300

    feat: creates list book details use case

---
+++ library-api/src/use-cases/list-book-details.ts
@@ -0,0 +1,23 @@
+import { BooksRepository } from '@/repositories/books-repository'
+import { Book } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+interface ListBookDetailsUseCaseRequest {
+  id: string
+}
+
+interface IListBookDetails {
+  run(params: ListBookDetailsUseCaseRequest): Promise<Book>
+}
+
+export class ListBookDetailsUseCase implements IListBookDetails {
+  constructor(private booksRepository: BooksRepository) {}
+
+  async run({ id }: ListBookDetailsUseCaseRequest) {
+    const book = await this.booksRepository.getBookDetails(id)
+    if (!book) {
+      throw new ResourceNotFoundError()
+    }
+    return book
+  }
+}
