e280651f15f0c278637938a5c843250a54920b24
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 18:31:24 -0300

    feat: creates a route to list book page by type

--- library-api/src/http/routes.ts
+++ library-api/src/http/routes.ts
@@ -1,26 +1,47 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import { listBooks } from './controllers/list-books'
 import { listBookDetails } from './controllers/list-book-details'
+import { listBookPageByType } from './controllers/list-book-page-by-type'
 
 export async function appRoutes(req: IncomingMessage, res: ServerResponse) {
   const { method } = req
   const baseURL = `http://${req.headers.host}`
   const parsedUrl = new URL(req.url || '', baseURL)
   const { pathname } = parsedUrl
-  const [apiSegment, v1Segment, booksSegment, bookId] = parsedUrl.pathname
-    .split('/')
-    .filter(Boolean)
+  const [
+    apiSegment,
+    v1Segment,
+    booksSegment,
+    bookId,
+    pageSegment,
+    pageNumber,
+    pageType,
+  ] = parsedUrl.pathname.split('/').filter(Boolean)
 
-  if (pathname === '/api/v1/books' && method === 'GET') {
-    await listBooks(req, res)
-  } else if (
+  const bookDetailsRouteCondition =
     req.method === 'GET' &&
     apiSegment === 'api' &&
     v1Segment === 'v1' &&
     booksSegment === 'books' &&
+    pageSegment !== 'page' &&
     bookId
-  ) {
+
+  const bookPageByTypeRouteCondition =
+    req.method === 'GET' &&
+    apiSegment === 'api' &&
+    v1Segment === 'v1' &&
+    booksSegment === 'books' &&
+    pageSegment === 'page' &&
+    bookId &&
+    pageNumber &&
+    pageType
+
+  if (pathname === '/api/v1/books' && method === 'GET') {
+    await listBooks(req, res)
+  } else if (bookDetailsRouteCondition) {
     await listBookDetails(req, res, bookId)
+  } else if (bookPageByTypeRouteCondition) {
+    await listBookPageByType(req, res, bookId, pageNumber, pageType)
   } else {
     res.writeHead(404)
     return res.end(JSON.stringify('Resource Not Found'))
