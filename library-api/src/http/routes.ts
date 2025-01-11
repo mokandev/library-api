e730b42cfa5adc6430b7add9231edfd4b712b7b0
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-11 17:28:52 -0300

    feat: implements route to books details

--- library-api/src/http/routes.ts
+++ library-api/src/http/routes.ts
@@ -1,15 +1,28 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import { listBooks } from './controllers/list-books'
+import { listBookDetails } from './controllers/list-book-details'
 
 export async function appRoutes(req: IncomingMessage, res: ServerResponse) {
   const { method } = req
   const baseURL = `http://${req.headers.host}`
-  const { pathname } = new URL(req.url || '', baseURL)
+  const parsedUrl = new URL(req.url || '', baseURL)
+  const { pathname } = parsedUrl
+  const [apiSegment, v1Segment, booksSegment, bookId] = parsedUrl.pathname
+    .split('/')
+    .filter(Boolean)
 
   if (pathname === '/api/v1/books' && method === 'GET') {
     await listBooks(req, res)
+  } else if (
+    req.method === 'GET' &&
+    apiSegment === 'api' &&
+    v1Segment === 'v1' &&
+    booksSegment === 'books' &&
+    bookId
+  ) {
+    await listBookDetails(req, res, bookId)
   } else {
     res.writeHead(404)
-    res.end(JSON.stringify('Resource Not Found'))
+    return res.end(JSON.stringify('Resource Not Found'))
   }
 }
