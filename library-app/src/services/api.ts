470bd4bcfbc9bcca57dac9b1a067f361382f136f
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 18:09:41 -0300

    feat: creates function to getBook and types service

--- library-app/src/services/api.ts
+++ library-app/src/services/api.ts
@@ -1,8 +1,25 @@
+import { IBook } from "../interfaces/book"
+
 const {VITE_API_URL} = import.meta.env
 
+export interface IGetBooks {
+  books: IBook[]
+}
 
-export async function getBooks() {
+export interface IGetBook {
+  book: IBook
+}
+
+
+export async function getBooks(): Promise<IGetBooks> {
   const res = await fetch(`${VITE_API_URL}/books`)
+  const { data } = await res.json()
+  return data
+}
+
+export async function getBook(bookId: string): Promise<IGetBook | undefined> {
+  const res = await fetch(`${VITE_API_URL}/books/${bookId}`)
+  if (!res.ok) throw Error("Couldn't find this book");
   const { data } = await res.json()
   return data
 }
