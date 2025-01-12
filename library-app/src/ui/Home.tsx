bd0836cd2f6b582d272958f7e0f7845db4696003
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 20:16:03 -0300

    styles: adjusting ui elements with flexbox

--- library-app/src/ui/Home.tsx
+++ library-app/src/ui/Home.tsx
@@ -2,9 +2,9 @@ import CreateUser from "../features/user/CreateUser";
 
 export default function Home() {
   return (
-    <div className="my-10 sm:my-16 text-center ">
-      <h1 className="mb-8 text-xl font-semibold">
-        Home
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="mb-8 text-xl font-semibold md:text-3xl">
+        Library
         <br />
         <span className="text-yellow-500">The online best library app</span>
       </h1>
