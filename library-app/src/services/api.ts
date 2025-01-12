cc0026b185942114f5d024361fe37562bc0d2b6e
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 18:29:39 -0300

    feat: add service to get book page

--- library-app/src/services/api.ts
+++ library-app/src/services/api.ts
@@ -1,4 +1,5 @@
 import { IBook } from "../interfaces/book"
+import { IPage, PageType } from "../interfaces/page"
 
 const {VITE_API_URL} = import.meta.env
 
@@ -10,7 +11,6 @@ export interface IGetBook {
   book: IBook
 }
 
-
 export async function getBooks(): Promise<IGetBooks> {
   const res = await fetch(`${VITE_API_URL}/books`)
   const { data } = await res.json()
@@ -20,6 +20,13 @@ export async function getBooks(): Promise<IGetBooks> {
 export async function getBook(bookId: string): Promise<IGetBook | undefined> {
   const res = await fetch(`${VITE_API_URL}/books/${bookId}`)
   if (!res.ok) throw Error("Couldn't find this book");
+  const { data } = await res.json()
+  return data
+}
+
+export async function getBookPage(bookId: string, pageNumber: number, textType: PageType ): Promise<IPage | undefined> {
+  const res = await fetch(`${VITE_API_URL}/books/${bookId}/page/${pageNumber}/${textType}`)
+  if (!res.ok) throw Error("Couldn't find this page");
   const { data } = await res.json()
   return data
 }
