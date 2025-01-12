b6cacff5f1fbfb0826e222dd6609aa86a20bf848
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 18:08:52 -0300

    refactor: types in library component

--- library-app/src/features/library/Library.tsx
+++ library-app/src/features/library/Library.tsx
@@ -1,19 +1,17 @@
-import { useLoaderData } from "react-router-dom";
-import { getBooks } from "../../services/api";
+import { useLoaderData } from 'react-router-dom';
+import { getBooks, IGetBooks } from '../../services/api';
+import { IBook } from '../../interfaces/book';
 
 export default function Library() {
-  const {books} =  useLoaderData()
-  console.log('booksList',books)
-  return (
-    <div>
-     <ul>
-      {books.map((book) => <li key={book.id}>{book.title}</li>)}
-     </ul>
-    </div>
-  )
+	const { books } = useLoaderData<IGetBooks>();
+	return (
+		<div>
+			<ul>{books.map((book: IBook) => <li key={book.id}>{book.title}</li>)}</ul>
+		</div>
+	);
 }
 
 export async function loader() {
-	const menu = await getBooks();
-	return menu;
+	const books = await getBooks();
+	return books;
 }
