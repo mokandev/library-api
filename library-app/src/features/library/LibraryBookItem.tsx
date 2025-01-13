d8e9b411c96bf85d1f59afda6fd70477ccac26ab
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-13 00:37:45 -0300

    styles: update LibraryBookItem styles

--- library-app/src/features/library/LibraryBookItem.tsx
+++ library-app/src/features/library/LibraryBookItem.tsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom"
-import { IBook } from "../../interfaces/book"
+import { Link } from 'react-router-dom';
+import { IBook } from '../../interfaces/book';
 
 interface ILibraryBookItemProps {
   book: IBook;
 }
 
-
-
-export const LibraryBookItem: React.FC<ILibraryBookItemProps> = ({ book })  => {
+export const LibraryBookItem: React.FC<ILibraryBookItemProps> = ({ book }) => {
   return (
-    <li>
-      <Link to={`/book/${book.id}`}>{book.title}</Link>
+    <li className="mb-2 py-2 text-xl">
+      <Link className="capitalize" to={`/book/${book.id}`}>
+        📖 {book.title}
+      </Link>
     </li>
-  )
-}
+  );
+};
