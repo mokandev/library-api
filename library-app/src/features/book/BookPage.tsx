cd650722b285e205350a8c779cd78e046df785b0
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 21:19:58 -0300

    feat: consume button in BookPage

--- library-app/src/features/book/BookPage.tsx
+++ library-app/src/features/book/BookPage.tsx
@@ -1,19 +1,35 @@
-import { LoaderFunctionArgs, useLoaderData } from "react-router-dom";
-import { getBookPage } from "../../services/api";
-import { IPage } from "../../interfaces/page";
+import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom';
+import { getBookPage } from '../../services/api';
+import { IPage } from '../../interfaces/page';
+import { Button } from '../../ui/Button';
 
 export default function BookPage() {
+  const { content } = useLoaderData<IPage>();
 
-  const {content} =  useLoaderData<IPage>()
+  function handleNextPageClick() {
+    console.log('handleNextPageClick');
+  }
+
+  function handlePreviousPageClick() {
+    console.log('handlePreviousPageClick');
+  }
+
   return (
     <div>
       <p>{content}</p>
+      <div>
+        <Button text="Previous Page" handlerFunction={handleNextPageClick} />
+        <Button text="Next Page" handlerFunction={handlePreviousPageClick} />
+      </div>
     </div>
-  )
+  );
 }
 
-
-export async function loader({params}: LoaderFunctionArgs) {
-  const bookPage = await getBookPage(params.bookId as string, Number(params.pageNumber), 'plain')
-  return bookPage
+export async function loader({ params }: LoaderFunctionArgs) {
+  const bookPage = await getBookPage(
+    params.bookId as string,
+    Number(params.pageNumber),
+    'plain',
+  );
+  return bookPage;
 }
