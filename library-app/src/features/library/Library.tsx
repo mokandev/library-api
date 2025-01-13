599374dc877b03ee7e4ffa8733dad763a84f8d3c
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-13 00:36:43 -0300

    styles: update styles of library component

--- library-app/src/features/library/Library.tsx
+++ library-app/src/features/library/Library.tsx
@@ -2,15 +2,14 @@ import { useLoaderData } from 'react-router-dom';
 import { getBooks, IGetBooks } from '../../services/api';
 import { IBook } from '../../interfaces/book';
 import { LibraryBookItem } from './LibraryBookItem';
+import { PageTitle } from '../../ui/PageTitle';
 
 export default function Library() {
   const { books } = useLoaderData<IGetBooks>();
   return (
     <div>
-      <h2 className="mb-8 text-center text-xl font-semibold md:text-2xl">
-        Library Book List
-      </h2>
-      <ul>
+      <PageTitle>Library Book List</PageTitle>
+      <ul className="divide-y divide-stone-200 px-2 border-b">
         {books.map((book: IBook) => (
           <LibraryBookItem key={book.id} book={book} />
         ))}
