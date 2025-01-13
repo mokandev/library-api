4a78c6f34210e5440df393dd2cdbb3229bcaa5d7
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 21:18:06 -0300

    feat: Creates a reusable button

---
+++ library-app/src/ui/Button.tsx
@@ -0,0 +1,15 @@
+interface IButtonProps {
+  text: string;
+  handlerFunction: () => void;
+}
+
+export const Button: React.FC<IButtonProps> = ({ text, handlerFunction }) => {
+  return (
+    <button
+      onClick={handlerFunction}
+      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-stone-500"
+    >
+      {text}
+    </button>
+  );
+};
