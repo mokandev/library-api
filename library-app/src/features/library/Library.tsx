1f4b2aea0874e2b4975f8e1a0249fec8583b1b22
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-12 21:18:41 -0300

    feat: add h2 to Library Component

--- library-app/src/features/library/Library.tsx
+++ library-app/src/features/library/Library.tsx
@@ -4,19 +4,22 @@ import { IBook } from '../../interfaces/book';
 import { LibraryBookItem } from './LibraryBookItem';
 
 export default function Library() {
-	const { books } = useLoaderData<IGetBooks>();
-	return (
-		<div>
-			<ul>
-				{books.map((book: IBook) => (
-					<LibraryBookItem key={book.id} book={book} />
-				))}
-			</ul>
-		</div>
-	);
+  const { books } = useLoaderData<IGetBooks>();
+  return (
+    <div>
+      <h2 className="mb-8 text-center text-xl font-semibold md:text-2xl">
+        Library Book List
+      </h2>
+      <ul>
+        {books.map((book: IBook) => (
+          <LibraryBookItem key={book.id} book={book} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export async function loader() {
-	const books = await getBooks();
-	return books;
+  const books = await getBooks();
+  return books;
 }
