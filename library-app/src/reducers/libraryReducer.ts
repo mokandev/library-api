45591282498c3a0c2b1b6acbc1c0935583ce7e8f
Carlos Afonso <carlos.flach15@gmail.com>
2025-01-13 03:18:37 -0300

    feat: update reducer to deal with book change

--- library-app/src/reducers/libraryReducer.ts
+++ library-app/src/reducers/libraryReducer.ts
@@ -3,16 +3,23 @@ import { IBook } from "../interfaces/book"
 interface IState {
   username: string
   books: IBook[]
+  currentBookPage: number
+  currentBookId: string
+  currentBookTotalPages: number
 }
 
 export const initialState:IState  = {
   username: "",
-  books: []
+  books: [],
+  currentBookPage: 0,
+  currentBookId: "",
+  currentBookTotalPages: 0
 }
 
+export type ActionTypes = "user/create" | 'books/load' | "book/updateCurrentBook" | "book/updateCurrentBookPage" | "book/nextPage" | "book/previousPage"
 interface IAction {
-  type: "user/create" | 'books/load'
-  payload?: string | IBook[]
+  type: ActionTypes
+  payload?: string | IBook[] | number
 }
 
 export const reducer = (state: IState, action: IAction): IState => {
@@ -29,6 +36,38 @@ export const reducer = (state: IState, action: IAction): IState => {
       return {
         ...state, 
         books
+      }
+    }
+
+    case "book/updateCurrentBook": {
+      const bookId = action.payload as string
+      const {total_pages: totalPages} = state.books.find((item) => item.id === bookId) as IBook
+      return {
+        ...state,
+        currentBookId: bookId,
+        currentBookPage: 0,
+        currentBookTotalPages: totalPages
+      }
+    }
+
+    case "book/updateCurrentBookPage": {
+      return {
+        ...state,
+        currentBookPage: action.payload as number
+      }
+    }
+
+    case "book/nextPage": {
+      return {
+        ...state,
+        currentBookPage: state.currentBookPage + 1
+      }
+    }
+
+    case "book/previousPage": {
+      return {
+        ...state,
+        currentBookPage: state.currentBookPage - 1
       }
     }
 
